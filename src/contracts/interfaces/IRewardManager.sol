90d3761c582928c2325594d16f31c2bc937ba468
seek <guoshijiang2012@163.com>
2024-08-07 13:21:12 +0800

    reward manager develop (#3)

--- src/contracts/interfaces/IRewardManager.sol
+++ src/contracts/interfaces/IRewardManager.sol
@@ -2,4 +2,25 @@
 pragma solidity ^0.8.0;
 
 interface IRewardManager {
+    event OperatorAndStakeReward(
+        address strategy,
+        address operator,
+        uint256 stakerFee,
+        uint256 operatorFee
+    );
+
+    event OperatorClaimReward (
+        address operator,
+        uint256 amount
+    );
+
+    event StakeHolderClaimReward(
+        address stakeHolder,
+        uint256 amount
+    );
+
+    function payFee(address strategy, address operator, uint256 baseFee) external;
+    function operatorClaimReward() external returns (bool);
+    function stakeHolderClaimReward(address strategy) external returns (bool);
+    function updateStakePercent(uint256 _stakePercent) external;
 }
